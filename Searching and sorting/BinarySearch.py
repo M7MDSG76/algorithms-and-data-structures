3dd69342610f05d94740917062c9d0cd916ce5fd
M7MDSG76 <mohammed.s.alghanmi@gmail.com>
2022-06-26 11:25:15 +0300

    Binary Search recursive methode

--- Searching and sorting/BinarySearch.py
+++ Searching and sorting/BinarySearch.py
@@ -3,7 +3,7 @@
 
 
 
-def binary_search(arr, element):
+def binary_search_iteration_methode(arr, element):
     high = len(arr)-1
     low = 0
     iterations = 0
@@ -18,11 +18,23 @@ def binary_search(arr, element):
         else:
             low = mid + 1           
     return 'Not found'
-            
+
+def binary_search_recursive_methode(arr, element, high, low):
+    mid = (low + high) // 2
+    while low<= high:
+        if element == arr[mid]:
+            return mid
+        elif element > arr[mid]:
+            return binary_search_recursive_methode(arr, element, high, low = mid + 1)    
+        else:
+            return binary_search_recursive_methode(arr, element, high = mid - 1, low = low)   
+        
+    return 'Not found'       
 arr = [1,2,3,4,5,6,7,8,9,22,423,434,554,666]
 
 
-x = 552
-print(binary_search(arr,x))
+x = 22
+print(binary_search_iteration_methode(arr,x))
+print( binary_search_recursive_methode(arr, x, len(arr)-1, 0))
 
 
